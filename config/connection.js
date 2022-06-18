636780265e32d0620c68fc12ea73e4ff4ad64186
Paul Haberle <61989020+phaberle@users.noreply.github.com>
2022-06-18 14:04:29 -0500

    updated connection and Category model

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,13 @@
+const e = require('express');
+const Sequalize = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
+const sequelize = new Sequalize('ecommerce_db', 'dbuser', 'dbuser',{
+  host:'localhost',
+  dialect: 'mysql',
+  port: '3306'
+});
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
 
 module.exports = sequelize;
+
